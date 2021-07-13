025b27ac3c18d0f7aac6162265af761d3cc585f7
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-07-13 11:53:13 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import requests
+import sys
+
+def getChangedFiles(TRAVIS_PULL_REQUEST):
+  response = requests.get("https://api.github.com/repos/Rohan-Shinde-98/test-pr-files/pulls/"+TRAVIS_PULL_REQUEST+"/files")
+  print(response)
+
+if __name__=="__main__":
+  getChangedFiles(sys.argv[1])
