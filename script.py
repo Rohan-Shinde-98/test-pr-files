6abd15c91b3b834afaad5bd8d9c211a4a244dc21
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-07-13 12:38:26 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,9 @@ import sys
 def getChangedFiles(TRAVIS_PULL_REQUEST):
   response = requests.get("https://api.github.com/repos/Rohan-Shinde-98/test-pr-files/pulls/"+str(TRAVIS_PULL_REQUEST)+"/files")
 #   print(response.text)
+  file = open('changed_files','a')
   for object in response.json():
+    file.write(object[filename'] + " " + object['raw_url'])
     print(object['filename'])
     print(object['raw_url'])
 
