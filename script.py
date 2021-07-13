3debb528581d093528ec31f03eb9eef1d2052531
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-07-13 13:38:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -9,12 +9,6 @@ def getChangedFiles(TRAVIS_PULL_REQUEST):
     string = object['filename'] + "\t" + object['raw_url']
     file.write(string)
   file.close()
-  file = open('changed_files.txt','r')
-  print(file.read())
-  file.close()
 
 if __name__=="__main__":
   getChangedFiles(sys.argv[1])
-  file = open('changed_files.txt','r')
-  print(file.read())
-  file.close()
