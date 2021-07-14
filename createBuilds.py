6f256305ed4f790a7757262454d2bf9e766e33a4
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-07-14 15:58:12 +0530

    Update createBuilds.py

--- createBuilds.py
+++ createBuilds.py
@@ -24,7 +24,7 @@ def triggerBuilds(file):
     "Authorization": "token " +  os.environ['AUTH_TOKEN']
   }
   
-  response = requests.post("https://api.travis-ci.com/repo/Rohan-Shinde-98%2Ftest-pr-files/requests", body=body, headers=headers)
+  response = requests.post("https://api.travis-ci.com/repo/Rohan-Shinde-98%2Ftest-pr-files/requests", data=body, headers=headers)
   print(response.text)
   
   
