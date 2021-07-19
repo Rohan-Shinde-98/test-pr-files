f07f5800c44ebf5768589efb702af3041c51f9df
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-07-19 18:35:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,20 +5,20 @@ def getChangedFiles(TRAVIS_PULL_REQUEST):
   response = requests.get("https://api.github.com/repos/Rohan-Shinde-98/test-pr-files/pulls/"+str(TRAVIS_PULL_REQUEST)+"/files")
   
   #remove line
-  print(response.json())
+  print(response.text)
   
   file = open('changed_files.txt','a')
   for object in response.json():
+    string = object['filename'] + "\t" + object['raw_url']                  
     
     #remove below lines
-    string = object['filename'] + "\t" + object['raw_url']
-    print(object['filename'], object['raw_url'])                       
     print(string)
     
     file.write(string)
   file.close()
   
   #remove below code
+  print("----------------------------------------------READING USING readline() FUNCTION----------------------------------")
   f = open('changed_files.txt','r')
   print(f.readline())
   f.close()
