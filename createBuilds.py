7f63cec83eceb5dbbf845b486a24c0b000c8e020
Rohan-Shinde-98 <Rohan.Shinde@ibm.com>
2021-07-21 07:55:53 +0000

    added threads

--- createBuilds.py
+++ createBuilds.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import threading
 
 def triggerBuilds(fileName, fileURL):
   body={
@@ -23,8 +24,22 @@ def triggerBuilds(fileName, fileURL):
   
   response = requests.post("https://api.travis-ci.com/repo/Rohan-Shinde-98%2Ftest-trigger-build/requests", data=json.dumps(body), headers=headers)
 
- 
-files = open('changed_files.txt','r')
-for file in files:
+
+# Hold the each build threads 
+build_threads = []
+
+file_content = open('changed_files.txt','r')
+for file in file_content:
   content = file.split()
   triggerBuilds(content[0], content[1])
+
+  # Create thread for each build
+  t = threading.Thread(target=triggerBuilds, arg=[fileName,fileURL])
+  t.start()
+  build_threads.append(t)
+
+# Wait for all child builds to finish
+for thread in build_threads:
+  thread.join()
+
+file_content.close()
