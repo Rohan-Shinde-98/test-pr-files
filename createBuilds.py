a8c7c2840e5b5ea3d19c2ecdcc2d7ee3b368d861
Rohan-Shinde-98 <Rohan.Shinde@ibm.com>
2021-07-21 08:07:26 +0000

    commented thread code

--- createBuilds.py
+++ createBuilds.py
@@ -24,6 +24,9 @@ def triggerBuilds(fileName, fileURL):
   
   response = requests.post("https://api.travis-ci.com/repo/Rohan-Shinde-98%2Ftest-trigger-build/requests", data=json.dumps(body), headers=headers)
 
+  # Polling
+  
+
 
 # Hold the each build threads 
 build_threads = []
@@ -33,13 +36,13 @@ for file in file_content:
   content = file.split()
   triggerBuilds(content[0], content[1])
 
-  # Create thread for each build
-  t = threading.Thread(target=triggerBuilds, arg=[fileName,fileURL])
-  t.start()
-  build_threads.append(t)
+#   # Create thread for each build
+#   t = threading.Thread(target=triggerBuilds, arg=[fileName,fileURL])
+#   t.start()
+#   build_threads.append(t)
 
-# Wait for all child builds to finish
-for thread in build_threads:
-  thread.join()
+# # Wait for all child builds to finish
+# for thread in build_threads:
+#   thread.join()
 
 file_content.close()
